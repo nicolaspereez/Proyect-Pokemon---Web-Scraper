68b13ac04efe4e98e80b680b426ada7f6c9c579b
nicolaspereez <92058498+nicolaspereez@users.noreply.github.com>
2022-03-11 09:49:59 -0300

    Refacción del proyecto pokemon
    Refacción del proyecto

--- pokemon.py
+++ pokemon.py
@@ -5,38 +5,45 @@ from bs4 import BeautifulSoup
 DOMAIN = 'https://pokemondb.net'
 URL = '/pokedex/all'
 
-if __name__ == '__main__':
-    response = requests.get(DOMAIN + URL)
+def get_content(url):
+    response = requests.get(url)
 
     if response.status_code == 200:
         content = response.text
 
         soup = BeautifulSoup(content, 'html.parser')
+        return soup
+    else:
+        return None
 
-        table = soup.find('table', {'id': 'pokedex'})
+def get_species_pokemon(url):
+    soup = get_content(url)
 
-        for row in table.tbody.find_all('tr', limit=5):
-            
+    table = soup.find('table', class_='vitals-table')
+    species = table.tbody.find_all('tr')[2].td.text
+
+    return species
+
+def show_pokemon_data():
+    soup = get_content(DOMAIN + URL)
+
+    table = soup.find('table', {'id': 'pokedex'})
+
+    for row in table.tbody.find_all('tr', limit=5):
             columns = row.find_all('td', limit=3)
 
             name= columns[1].a.text
             type = [a.text for a in columns[2].find_all('a')]
             link = DOMAIN + columns[1].a['href']
 
-            
-            pokemon_response = requests.get(link)
+            species = get_species_pokemon(link)
 
-            if pokemon_response.status_code == 200:
-                pokemon_content = pokemon_response.text
+            print(name, '-', *type, '-', species )
+
+if __name__ == '__main__':
+    show_pokemon_data()
+
                 
-                pokemoon_soup = BeautifulSoup(pokemon_content, 'html.parser')
-
-                pokemon_table = pokemoon_soup.find('table', 
-                                                    class_='vitals-table')
-                
-                species = pokemon_table.tbody.find_all('tr')[2].td.text
-
-                print(name, '-', *type, '-', species)
 
         
 
